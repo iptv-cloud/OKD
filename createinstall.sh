e736bd7bd115765073b4580166527cf79f8fa951
iptv-cloud <95514761+iptv-cloud@users.noreply.github.com>
2022-10-06 10:35:52 -0400

    Update createinstall.sh

--- createinstall.sh
+++ createinstall.sh
@@ -1,1 +1,4 @@
 sudo coreos-installer install --ignition-url=http://192.168.1.2:80/installation_directory/bootstrap.ign /dev/sda --ignition-hash=sha512-a5a2d43879223273c9b60af66b44202a1d1248fc01cf156c46d4a79f552b6bad47bc8cc78ddf0116e80c59d2ea9e32ba53bc807afbca581aa059311def2c3e3b
+
+
+sha512sum <installation_directory>/bootstrap.ign
